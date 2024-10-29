5089f6399ace6a523c22588f3361a0140814d6ad
Vana Stergiou <vanastergiou@gmail.com>
2024-10-29 19:24:57 +0200

    Update 2.0_line.php

--- 2.0_line.php
+++ 2.0_line.php
@@ -21,123 +21,109 @@
 
 	<!--[if lt IE 9]> <script src="assets/js/html5shiv.js"></script> <![endif]-->
 </head>
+
 <body>
 
-<?php 
-$van='gr2';
-include 'menu.php'; ?>
+	<?php 
+		$van='gr2';
+		include 'menu.php'; 
+	?>
 
-<main id="main">
+	<main id="main">
 
-	<div class="container">
+		<div class="container">
 
-		<div class="row topspace">
-			<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-3 col-sm-4 sidebar2">
-                <div class="logo">
-                    <img src="assets\images\g2.png" width="100px" class="img-responsive center-block" alt="Logo">
-                </div>
-                <br>
-				
-                <div class="left-navigation">
-                    <ul class="list">
-                        <li id="25" onclick="gothere(this)"><a href="#">ΑΝΘΟΥΠΟΛΗ</a></li>
-                        <li id="26" onclick="gothere(this)"><a href="#">ΠΕΡΙΣΤΕΡΙ</a></li>
-                        <li id="27" onclick="gothere(this)"><a href="#">ΑΓΙΟΣ ΑΝΤΩΝΙΟΣ</a></li>
-                        <li id="28" onclick="gothere(this)"><a href="#">ΣΕΠΟΛΙΑ</a></li>
-                        <li id="11" onclick="gothere(this)"><a href="#">ΑΤΤΙΚΗ</a></li>
-                        <li id="29" onclick="gothere(this)"><a href="#">ΣΤΑΘΜΟΣ ΛΑΡΙΣΗΣ</a></li>
-                        <li id="30" onclick="gothere(this)"><a href="#">ΜΕΤΑΞΟΥΡΓΕΙΟ</a></li>
-                        <li id="9" onclick="gothere(this)"><a href="#">ΟΜΟΝΟΙΑ</a></li>
-                        <li id="31" onclick="gothere(this)"><a href="#">ΠΑΝΕΠΙΣΤΗΜΙΟ</a></li>
-                        <li id="32" onclick="gothere(this)"><a href="#">ΣΥΝΤΑΓΜΑ</a></li>
-                        <li id="33" onclick="gothere(this)"><a href="#">ΑΚΡΟΠΟΛΗ</a></li>
-                        <li id="34" onclick="gothere(this)"><a href="#">ΣΥΓΓΡΟΥ ΦΙΞ</a></li>
-						<li id="35" onclick="gothere(this)"><a href="#">ΝΕΟΣ ΚΟΣΜΟΣ</a></li>
-                        <li id="36" onclick="gothere(this)"><a href="#">ΑΓΙΟΣ ΙΩΑΝΝΗΣ</a></li>
-                        <li id="37" onclick="gothere(this)"><a href="#">ΔΑΦΝΗ</a></li>
-                        <li id="38" onclick="gothere(this)"><a href="#">ΑΓΙΟΣ ΔΗΜΗΤΡΙΟΣ</a></li>
-                        <li id="39" onclick="gothere(this)"><a href="#">ΗΛΙΟΥΠΟΛΗ</a></li>
-                        <li id="40" onclick="gothere(this)"><a href="#">ΑΛΙΜΟΣ</a></li>
-                        <li id="41" onclick="gothere(this)"><a href="#">ΑΡΓΥΡΟΥΠΟΛΗ</a></li>
-                        <li id="42" onclick="gothere(this)"><a href="#">ΕΛΛΗΝΙΚΟ</a></li>
-                       
-                    </ul>
-                </div>
-				
-            </div>
-            <div class="col-md-8 col-sm-8 main-content">
-            <!--Main content code to be written here --> 
-             <h1 style="color:#591b21; text-align: center;" >ΑΝΘΟΥΠΟΛΗ - ΕΛΛΗΝΙΚΟ</h1>
-            
-			
-			
-			</br>
-			 <div class="col-md-12">
-				<div id="myCarousel" class="carousel slide" data-ride="carousel">
-					  <!-- Indicators -->
-					  <ol class="carousel-indicators">
-						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-						<li data-target="#myCarousel" data-slide-to="1"></li>
-						<li data-target="#myCarousel" data-slide-to="2"></li>
-					  </ol>
-
-					  <!-- Wrapper for slides -->
-					  <div class="carousel-inner">
-												  
-							  
-							<div class="item active">
-							  <img src="assets\images\cl-red-1a.jpg" alt="Chania">
-							  <div class="carousel-caption">
-								</div>
+			<div class="row topspace">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-md-3 col-sm-4 sidebar2">
+							<div class="logo">
+								<img src="assets\images\g2.png" width="100px" class="img-responsive center-block" alt="Logo">
 							</div>
+							<br>
+							<div class="left-navigation">
+								<ul class="list">
+									<li id="25" onclick="gothere(this)"><a href="#">ΑΝΘΟΥΠΟΛΗ</a></li>
+									<li id="26" onclick="gothere(this)"><a href="#">ΠΕΡΙΣΤΕΡΙ</a></li>
+									<li id="27" onclick="gothere(this)"><a href="#">ΑΓΙΟΣ ΑΝΤΩΝΙΟΣ</a></li>
+									<li id="28" onclick="gothere(this)"><a href="#">ΣΕΠΟΛΙΑ</a></li>
+									<li id="11" onclick="gothere(this)"><a href="#">ΑΤΤΙΚΗ</a></li>
+									<li id="29" onclick="gothere(this)"><a href="#">ΣΤΑΘΜΟΣ ΛΑΡΙΣΗΣ</a></li>
+									<li id="30" onclick="gothere(this)"><a href="#">ΜΕΤΑΞΟΥΡΓΕΙΟ</a></li>
+									<li id="9" onclick="gothere(this)"><a href="#">ΟΜΟΝΟΙΑ</a></li>
+									<li id="31" onclick="gothere(this)"><a href="#">ΠΑΝΕΠΙΣΤΗΜΙΟ</a></li>
+									<li id="32" onclick="gothere(this)"><a href="#">ΣΥΝΤΑΓΜΑ</a></li>
+									<li id="33" onclick="gothere(this)"><a href="#">ΑΚΡΟΠΟΛΗ</a></li>
+									<li id="34" onclick="gothere(this)"><a href="#">ΣΥΓΓΡΟΥ ΦΙΞ</a></li>
+									<li id="35" onclick="gothere(this)"><a href="#">ΝΕΟΣ ΚΟΣΜΟΣ</a></li>
+									<li id="36" onclick="gothere(this)"><a href="#">ΑΓΙΟΣ ΙΩΑΝΝΗΣ</a></li>
+									<li id="37" onclick="gothere(this)"><a href="#">ΔΑΦΝΗ</a></li>
+									<li id="38" onclick="gothere(this)"><a href="#">ΑΓΙΟΣ ΔΗΜΗΤΡΙΟΣ</a></li>
+									<li id="39" onclick="gothere(this)"><a href="#">ΗΛΙΟΥΠΟΛΗ</a></li>
+									<li id="40" onclick="gothere(this)"><a href="#">ΑΛΙΜΟΣ</a></li>
+									<li id="41" onclick="gothere(this)"><a href="#">ΑΡΓΥΡΟΥΠΟΛΗ</a></li>
+									<li id="42" onclick="gothere(this)"><a href="#">ΕΛΛΗΝΙΚΟ</a></li>
+								</ul>
+							</div>	
+						</div>
 
-							<div class="item">
-							  <img src="assets\images\cl-red-2a.jpg" alt="Chicago">
-							  <div class="carousel-caption">
-							  </div>
+						<div class="col-md-8 col-sm-8 main-content">
+							<!--Main content code to be written here --> 
+							<h1 style="color:#591b21; text-align: center;" >ΑΝΘΟΥΠΟΛΗ - ΕΛΛΗΝΙΚΟ</h1>
+							</br>
+							<div class="col-md-12">
+								<div id="myCarousel" class="carousel slide" data-ride="carousel">
+									<!-- Indicators -->
+									<ol class="carousel-indicators">
+										<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+										<li data-target="#myCarousel" data-slide-to="1"></li>
+										<li data-target="#myCarousel" data-slide-to="2"></li>
+									</ol>
+									<!-- Wrapper for slides -->
+									<div class="carousel-inner">
+										<div class="item active">
+											<img src="assets\images\cl-red-1a.jpg" alt="Chania">
+											<div class="carousel-caption">
+											</div>
+										</div>
+										<div class="item">
+											<img src="assets\images\cl-red-2a.jpg" alt="Chicago">
+											<div class="carousel-caption">
+										</div>
+										</div>
+										<div class="item">
+											<img src="assets\images\cl-red-3a.jpg" alt="Chicago">
+											<div class="carousel-caption">
+										</div>
+										</div>
+									</div>
+								</div>  
 							</div>
-							
-							<div class="item">
-							  <img src="assets\images\cl-red-3a.jpg" alt="Chicago">
-							  <div class="carousel-caption">
-							  </div>
-							</div>
-
-					   </div>
-      
-				</div>  
+							</br>
+							</br>
+						
+							<p>Η Γραμμή 2 του μετρό της Αθήνας είναι η γραμμή Ανθούπολη - Ελληνικό, η οποία στους χάρτες απεικονίζεται με κόκκινο χρώμα. Ολόκληρη η γραμμή είναι υπόγεια και έχει διεύθυνση Βορρά - Νότου. Εγκαινιάστηκε στις 28 Ιανουαρίου 2000 μαζί με την Γραμμή 3 και αρχικά ένωνε τα Σεπόλια με την πλατεία Συντάγματος. Σήμερα, ξεκινάει από το Περιστέρι, πολυπληθές δυτικό προάστιο της Αθήνας, διέρχεται από τα Σεπόλια και το κέντρο της Αθήνας (Ομόνοια και Σύνταγμα), συνεχίζει νότια μέσα από το Νέο Κόσμο και τη Δάφνη και προχωρά κάτω από τη Λεωφόρο Βουλιαγμένης, για να καταλήξει στο Ελληνικό. Η γραμμή κατασκευάστηκε τμηματικά. Τον Ιανουάριο του 2000 παραδόθηκε το τμήμα Σεπόλια - Σύνταγμα και τον Νοέμβριο του ίδιου έτους το τμήμα Σύνταγμα - Δάφνη. Το 2004 δόθηκαν σε κυκλοφορία οι σταθμοί Άγιος Δημήτριος και Άγιος Αντώνιος. Η τελευταία μεγάλη επέκταση της γραμμής ήταν το 2013 όταν και παραδόθηκαν σε κυκλοφορία οι νέες προεκτάσεις προς Ανθούπολη και Ελληνικό.</p>
+						</div>
+					</div>
+				</div>
 			</div>
-			</br>
-			</br>
-			
-			
-			<p>Η Γραμμή 2 του μετρό της Αθήνας είναι η γραμμή Ανθούπολη - Ελληνικό, η οποία στους χάρτες απεικονίζεται με κόκκινο χρώμα. Ολόκληρη η γραμμή είναι υπόγεια και έχει διεύθυνση Βορρά - Νότου. Εγκαινιάστηκε στις 28 Ιανουαρίου 2000 μαζί με την Γραμμή 3 και αρχικά ένωνε τα Σεπόλια με την πλατεία Συντάγματος. Σήμερα, ξεκινάει από το Περιστέρι, πολυπληθές δυτικό προάστιο της Αθήνας, διέρχεται από τα Σεπόλια και το κέντρο της Αθήνας (Ομόνοια και Σύνταγμα), συνεχίζει νότια μέσα από το Νέο Κόσμο και τη Δάφνη και προχωρά κάτω από τη Λεωφόρο Βουλιαγμένης, για να καταλήξει στο Ελληνικό. Η γραμμή κατασκευάστηκε τμηματικά. Τον Ιανουάριο του 2000 παραδόθηκε το τμήμα Σεπόλια - Σύνταγμα και τον Νοέμβριο του ίδιου έτους το τμήμα Σύνταγμα - Δάφνη. Το 2004 δόθηκαν σε κυκλοφορία οι σταθμοί Άγιος Δημήτριος και Άγιος Αντώνιος. Η τελευταία μεγάλη επέκταση της γραμμής ήταν το 2013 όταν και παραδόθηκαν σε κυκλοφορία οι νέες προεκτάσεις προς Ανθούπολη και Ελληνικό.</p>
-        </div>
-    </div>
-</body>
+
+		</div>	<!-- /container -->
 	
+	</main>
 
-		</div>
-	</div>	<!-- /container -->
+	<?php include 'footer.php'; ?>
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="assets/js/template.js"></script>
+	<script>
+		function gothere(el) {
+			var str = el.getAttribute("id");
+			window.location.href = "protasi2.php?var=" + str;
+		}
+	</script>
 	
-</main>
-
-<?php include 'footer.php'; ?>
-
-
-
-<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="assets/js/template.js"></script>
-<script>
-function gothere(el) {
-	var str = el.getAttribute("id");
-	window.location.href = "protasi2.php?var=" + str;
-}
-</script>
 </body>
 </html>
